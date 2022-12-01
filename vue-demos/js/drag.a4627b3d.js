95f0cb561daf4ef77ac5f48157373d5b46a3c2b7
sunzsh <cn.xiaoshan@gmail.com>
2022-12-01 23:05:08 +0800

    vuedemo增加drag

---
+++ vue-demos/js/drag.a4627b3d.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["drag"],{2031:function(t,a,n){"use strict";n("d632")},d632:function(t,a,n){},f1c5:function(t,a,n){"use strict";n.r(a);var e=function(){var t=this,a=t._self._c;return a("div",[a("div",{staticClass:"some-panel"},[a("div",{directives:[{name:"drag",rawName:"v-drag"}],staticStyle:{height:"20px","background-color":"blue"},attrs:{"drag-min-top":"50"}})])])},i=[],r={},s=r,c=(n("2031"),n("2877")),d=Object(c["a"])(s,e,i,!1,null,"3f0388f4",null);a["default"]=d.exports}}]);
+//# sourceMappingURL=drag.a4627b3d.js.map
