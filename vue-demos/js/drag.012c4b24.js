334953cd4c054e62d4132c15cf99ca03bc367b16
sunzsh <cn.xiaoshan@gmail.com>
2023-05-04 20:12:35 +0800

    解决v-drag带margin导致bug的问题

---
+++ vue-demos/js/drag.012c4b24.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["drag"],{"931f":function(t,a,e){},f1c5:function(t,a,e){"use strict";e.r(a);var n=function(){var t=this,a=t._self._c;return a("div",[a("div",{staticClass:"some-panel"},[a("div",{directives:[{name:"drag",rawName:"v-drag"}],staticStyle:{height:"20px","background-color":"blue"},attrs:{"drag-min-top":"50"}})])])},i=[],c={},r=c,s=(e("f4f3"),e("2877")),o=Object(s["a"])(r,n,i,!1,null,"aecabd24",null);a["default"]=o.exports},f4f3:function(t,a,e){"use strict";e("931f")}}]);
+//# sourceMappingURL=drag.012c4b24.js.map
