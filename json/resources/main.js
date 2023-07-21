3c134661efef18b2e618a517bae8ba893ca0d29b
sunzsh <cn.xiaoshan@gmail.com>
2023-07-21 21:04:03 +0800

    json -> PWA版本

--- json/resources/main.js
+++ json/resources/main.js
@@ -16,9 +16,27 @@ async function init() {
   let json = ''
   try {
     try{
-      json = await localforage.getItem(jsonkey) || json
+      // 获取url后面的json字符串
+      if (!mode || mode == '') {
+        json = await localforage.getItem(jsonkey) || json
+      } else if ('none' == mode) {
+        json = ''
+      } else if ('clipboard' == mode) {
+          navigator.clipboard.readText()
+          .then(clipText => editor.set(JSON.parse(clipText)))
+          .catch(err => {
+            const msg = err + ''
+            console.error(msg)
+            if (msg.indexOf('permission') > -1) {
+              toast('bad','缺少剪切板权限')
+            } else if (msg.indexOf('focused') > -1) {
+              toast('bad','浏览器没有获取焦点，无法获取剪切板内容')
+            }
+          })
+        return
+      }
     }catch(e) {
-     
+     json = await localforage.getItem(jsonkey) || json
     }
   } catch (e) { }
   if (json) { 
